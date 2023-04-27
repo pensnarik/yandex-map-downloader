0a5c3d5cddbc065d8bf7eb48fb1e1e57747bb06f
Zhidenkov, Andrey <andrey.zhidenkov@aliexpress.ru>
2023-04-27 14:43:09 +0300

    Add args

--- yandex-map-downloader.py
+++ yandex-map-downloader.py
@@ -3,6 +3,7 @@
 import sys
 import json
 import logging
+import argparse
 
 from ymaps.tile import Tile
 from ymaps.map import Map, Coordinate, Borders
@@ -17,19 +18,57 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO, stream=sys.stdout)
 
 
-data = json.loads(open('moscow.json').read())
+class App():
 
-Borders = Borders(
-	Coordinate(data['coord1']['lat'], data['coord1']['lon']),
-	Coordinate(data['coord2']['lat'], data['coord2']['lon'])
-)
+    map = None
 
-map = Map(Borders, z=20, layer='sat', version=VERSION)
+    def __parse_args(self):
+        parser = argparse.ArgumentParser(description='Schaeffler store parser')
+        parser.add_argument('--map', help='Map filename', type=str, required=True)
+        parser.add_argument('--download', help='Download map', action='store_true', default=False)
+        parser.add_argument('--preview', help='Preview map', action='store_true', default=False)
+        self.args = parser.parse_args()
 
-logger.info(f"{len(map)=}")
 
-# map.download_scheduler = DownloadSleepScheduler
-# map.download()
+    def __init__(self):
+        self.__parse_args()
 
-renderer = PreviewRenderer(map)
-renderer.render('overview.png')
+
+    def download(self, filename):
+        self.map = self.prepare_map(filename)
+
+        self.map.download_scheduler = DownloadSleepScheduler
+        self.map.download()
+
+
+    def prepare_map(self, filename):
+        data = json.loads(open(filename).read())
+
+        borders = Borders(
+            Coordinate(data['coord1']['lat'], data['coord1']['lon']),
+            Coordinate(data['coord2']['lat'], data['coord2']['lon'])
+        )
+
+        return Map(borders, z=20, layer='sat', version=VERSION)
+
+        logger.info(f"{len(self.map)=}")
+
+
+    def preview(self, filename):
+        if not self.map:
+            self.map = self.prepare_map(filename)
+
+        renderer = PreviewRenderer(self.map)
+        renderer.render('overview.png')
+
+
+    def run(self):
+        if self.args.download:
+            self.download(self.args.map)
+        if self.args.preview:
+            self.preview(self.args.map)
+
+
+if __name__ == '__main__':
+    app = App()
+    sys.exit(app.run())
